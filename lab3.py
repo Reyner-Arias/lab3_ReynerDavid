f276f7bf0f4c04bfa66605dd2c570e030b47bc06
Reyner-Arias <Rey86581@gmail.com>
2022-09-17 01:23:14 -0600

    Calculate borders finished

--- lab3.py
+++ lab3.py
@@ -1,5 +1,5 @@
 # Authors: 
-# David Benavides Naranjo - ?
+# David Benavides Naranjo - 2019018297
 # Reyner Marxell Arias Muñoz - 2019156290
 # Date: 16-9-2022
 # Course: IC-8046 Introduction to Pattern Recognition
@@ -8,25 +8,46 @@
 import cv2
 import pathlib
 import math
+from matplotlib import pyplot as plt
 import numpy
+
+limit = 22
 
 print("Welcome to Lab03, please type the image path:")
 imagePath = input()
 
 image = cv2.imread(str(imagePath), cv2.IMREAD_GRAYSCALE)
-height, width, numberChannels = image.shape
+height, width = image.shape
 
-xAxis = numpy.random.randint(0, size=(height, width)).astype('uint8')
-yAxis = numpy.random.randint(0, size=(height, width)).astype('uint8')
-superpose = numpy.random.randint(0, size=(height, width)).astype('uint8')
+xAxis = image.copy()
+yAxis = image.copy()
+superpose = image.copy()
 
-grayXAxis = cv2.cvtColor(xAxis, cv2.COLOR_GRAY2BGR)
-grayYAxis = cv2.cvtColor(yAxis, cv2.COLOR_GRAY2BGR)
-graySuperpose = cv2.cvtColor(superpose, cv2.COLOR_GRAY2BGR)
+for i in range(0, height):
+    for j in range(0, width):
+        if j == 0 or i == 0:
+            xAxis[i][j] = 0
+            yAxis[i][j] = 0
+        else:
+            substractionY = abs(int(image[i][j])-int(image[i-1][j]))
+            if substractionY >= limit:
+                yAxis[i][j] = 255
+            else:
+                yAxis[i][j] = 0
+            
+            substractionX = abs(int(image[i][j])-int(image[i][j-1]))
+            if substractionX >= limit:
+                xAxis[i][j] = 255
+            else:
+                xAxis[i][j] = 0
 
-for i in range(0, len(image)):
-    i = 0
+for i in range(0, height):
+    for j in range(0, width):
+        if int(xAxis[i][j]) == int(yAxis[i][j]):
+            superpose[i][j] = 0
+        else:
+            superpose[i][j] = 255
 
-"""cv2.imshow("Prueba", image)
-cv2.waitKey(0)"""
-
+cv2.imwrite("xAxis.jpg", xAxis)
+cv2.imwrite("yAxis.jpg", yAxis)
+cv2.imwrite("superpose.jpg", superpose)
